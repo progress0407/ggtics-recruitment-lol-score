0689ebd17906ee2d22f837fb9f7942d9daeef69b
progress0407 <progress0407@gmail.com>
2023-01-11 21:25:45 +0900

    test(PreProcessor): 테스트 작성

--- ggtics-recruitement-lol-score/src/test/kotlin/com/your/lol/support/GlobalTestUtils.kt
+++ ggtics-recruitement-lol-score/src/test/kotlin/com/your/lol/support/GlobalTestUtils.kt
@@ -3,20 +3,30 @@ package com.your.lol.support
 import com.google.gson.reflect.TypeToken
 import com.your.lol.dto.riot.MatchDto
 import com.your.lol.domain.ChampionDataDto
+import com.your.lol.domain.dto.PreProcessedData
 import com.your.lol.dto.riot.StatDto
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.data.Offset.offset
 import org.junit.jupiter.api.function.Executable
+import kotlin.streams.toList
 
 
 abstract class GlobalTestUtils {
 
+    companion object {
+        fun List<PreProcessedData>.findChampionsByName(championName: String): List<PreProcessedData> {
+            return this.stream()
+                    .filter { preProcessData -> preProcessData.championName == championName }
+                    .toList()
+        }
+    }
+
     protected fun <T> assertEquals(actual: T, expected: T): Executable {
-        return Executable { Assertions.assertThat(actual).isEqualTo(expected) }
+        return Executable { assertThat(actual).isEqualTo(expected) }
     }
 
     protected fun <T> assertCloseTo(actual: Double, expected: Double, offset: Double): Executable {
-        return Executable { Assertions.assertThat(actual).isCloseTo(expected, offset(offset)) }
+        return Executable { assertThat(actual).isCloseTo(expected, offset(offset)) }
     }
 
     protected fun findChampionByName(statDto: StatDto, championName: String): ChampionDataDto {
